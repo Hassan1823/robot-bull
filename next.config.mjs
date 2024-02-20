baf9f58078c89b922b85f31291fda249d7ef3615
Hassan Zaib <snoopyhassan007@gmail.com>
2024-02-21 02:40:50 +0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["https://picsum.photos/id/10/300/300"],
+  },
+};
+
+export default nextConfig;
---
+++ notes.md
@@ -0,0 +1,4 @@
+color schema {
+color1 : #253992,
+color1 : #3e2bc5,
+}
